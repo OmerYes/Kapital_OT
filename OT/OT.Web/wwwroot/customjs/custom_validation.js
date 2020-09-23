3b930163b292d47eb76549f7d7870e41544d66ad
OmerYes <48590724+OmerYes@users.noreply.github.com>
2020-09-24 00:37:10 +0300

    -login ekranı validation eklendi

---
+++ OT/OT.Web/wwwroot/customjs/custom_validation.js
@@ -0,0 +1,22 @@
+﻿jQuery(function ($)
+{
+    $("#login_form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            password: {
+                required: true,
+            }
+        },
+
+        messages: {
+            email: {
+                required: "**Lütfen email adresinizi giriniz",
+                email:"**Lütfen geçerli bir mail adresi giriniz"
+            },
+            password:"**Lütfen şifrenizi giriniz"
+        }
+    })
+})
